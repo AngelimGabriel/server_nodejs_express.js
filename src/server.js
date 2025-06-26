2ea3d030d6e95e53481c68634e03b37da8488e42
Gabriel Angelim <gabriel_angelim@outlook.com>
2025-06-26 07:55:59 -0300

    Atualizações

--- src/server.js
+++ src/server.js
@@ -1,2 +1,7 @@
 import app from "./app.js";
-app.listen(3000);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`servidor esta rodando na porta ${PORT}`);
+});
